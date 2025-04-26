fa292417a8b3857f5b251b3d559b47dd3c945808
qlib4451 <qlib4451@gmail.com>
2025-04-26 12:36:10 -0400

    Update engine.json.php

--- engine.json.php
+++ engine.json.php
@@ -1,1 +1,12 @@
-
+  {
+  "id": "0",
+  "name": "BARRIO LOS PADRINOS 13",
+  "color": "042285",
+  "thread": "https://sheriff.sarp.es/viewtopic.php?t=997&sid=3ce42b0c8fb71ace79495972e7ff4321",
+  "turfIDs": ["2186"],
+  "turfs": ["5224 4814 5404 4814 5408 4722 5222 4724"],
+  "turfcenter": [[5314.5,4768.5 ]],
+  "turfPolygon": [],
+  "dimensions": [[186,92]],
+  "limits": [{"minx": 5222,"miny": 4722,"maxx": 5408,"maxy": 4814
+  },
