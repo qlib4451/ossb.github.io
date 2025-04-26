ff1fe56f911edd8501dc842e12369f8f6f46c8e1
qlib4451 <qlib4451@gmail.com>
2025-04-26 12:18:00 -0400

    Update engine.json.php

--- engine.json.php
+++ engine.json.php
@@ -1,36 +1,36 @@
 {
   "pandillas": [
     {
- {
-   "id": 3656,
-   "name": "BARRIO LOS PADRINOS 13",
-   "color": "0d13d7",
-   "thread": "https://sheriff.sarp.es/viewtopic.php?t=997&sid=3ce42b0c8fb71ace79495972e7ff4321",
-   "turfIDs": [
-     "3656"
-   ],
-   "turfs": [
-     "5226 4837 5408 4831 5408 4723 5222 4727"
-   ],
-   "turfcenter": [
-     [
-       5316,
-       4779.5
-     ]
-   ],
-   "turfPolygon": [],
-   "dimensions": [
-     [
-       186,
-       114
-     ]
-   ],
-   "limits": [
-     {
-       "minx": 5222,
-       "miny": 4723,
-       "maxx": 5408,
-       "maxy": 4837
-     }
-   ]
- }
+{
+"id": 3656,
+"name": "BARRIO LOS PADRINOS 13",
+"color": "0d13d7",
+"thread": "https://sheriff.sarp.es/viewtopic.php?t=997&sid=3ce42b0c8fb71ace79495972e7ff4321",
+"turfIDs": [
+"3656"
+],
+"turfs": [
+"5226 4837 5408 4831 5408 4723 5222 4727"
+],
+"turfcenter": [
+[
+5316,
+4779.5
+]
+],
+"turfPolygon": [],
+"dimensions": [
+[
+186,
+114
+]
+],
+"limits": [
+{
+"minx": 5222,
+"miny": 4723,
+"maxx": 5408,
+"maxy": 4837
+}
+]
+}
